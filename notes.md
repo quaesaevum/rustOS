7589e2f2ede219331b3977770b99885bc713dd7e
Josiah A. Roelfsema <jaroelfsema@gmail.com>
2020-09-09 08:44:19 -0500

    more formatting

--- notes.md
+++ notes.md
@@ -1,10 +1,10 @@
 # A Freestanding Rust Binary
 Personal notes of Josiah on following Philipp Oppermann's blog "Writing an OS in Rust", located at https://os.phil-opp.com/
 
-03 Sept 2020:
+### 03 Sept 2020:
 - Completed *A Freestanding Rust Binary*. Got everything working so far.
 
-05 Sept 2020:
+### 05 Sept 2020:
 - Started *A Minimal Rust Kernel*
 - Completed it as well
 Thoughts:  
@@ -14,12 +14,12 @@ Thoughts:
 - Could this run on a Pi? On an Arduino?
 - How could this work on IoT devices?
 
-07 Sept 2020:
+### 07 Sept 2020:
 Start *VGA Text Mode*:
 - VGA text buffer standard is 25 rows and 80 columns - just so, Terminal default is 80x24.
 - Hmm, what do you know, it works so far.
   - Got the VGA buffer up and running in basic form.
-### Semantic Versioning
+#### Semantic Versioning
 https://semver.org
 "Given a version number MAJOR.MINOR.PATCH, increment the:
 1. MAJOR version when you make incompatible API changes,
@@ -32,3 +32,16 @@ Ok, what did we learn so far?
 - We accessed the VGA buffer to write to std output. This was done by direct memory access to the VGA buffer using "unsafe" Rust. For now, we compile and run via QEMU emulator to keep this off our own computer, but it should work on a bare metal system with BIOS, bootloader, and VGA buffer.
 - VGA buffer has some ASCII characters, 16 colors for foreground, 8 colors for background, and blink option
 - VGA prints unknown UTF-8 as two nonsense characters due to UTF-8 being 32-bit instead of 16.
+
+### 08 Sept 2020:
+Continuing *VGA Text Mode*:  
+Finished.  
+What did we learn?  
+- Stuff about writing macros
+
+#### Started *Testing*
+https://wiki.osdev.org
+Built a basic trivial test without std:
+- Had to set up our own system of testing that mimics normal std testing
+- Built a unique set of exit codes to avoid confusion with QEMU's exit codes
+Completed up to *Printing to the Console* subheading  
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 #![no_std]  // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)] // allows custom tests instead of using std which we disabled
+#![test_runner(crate::test_runner)] // does something to bring in the test runner I think
+#![reexport_test_harness_main = "test_main"]    // allows use of the test_main() function
 
 extern crate rlibc;
 
@@ -9,28 +12,52 @@ use core::panic::PanicInfo;
 
 mod vga_buffer; // define a module for vga buffer - located in src/vga_buffer.rs
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
 
-//static HELLO: &[u8] = b"Hello World!";
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 #[no_mangle]    // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-/*    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-*/
-
-    vga_buffer::print_something();
+    #[cfg(test)]
+    test_main();    // invoke tests
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {   // runs tests for our custom setup
+    println!("Running {} tests", tests.len());  // prints text plus the length of the tests array
+    for test in tests {     // appears to collect all tests labeled via #[test_case] then loops
+        test();             // through each
+    }
+    
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]                // identifies a test
+fn trivial_assertion() {    // this is a trivial test to check our functionality
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
